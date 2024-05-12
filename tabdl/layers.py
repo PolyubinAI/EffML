ad82470470756b37ea022f7461d730e7a6909ad2
TrandeLik <feoctistov1580@yandex.ru>
2024-05-12 21:19:29 +0300

    tab dl utils and baselines

---
+++ tabdl/layers.py
@@ -0,0 +1,474 @@
+"""Adopted from https://github.com/yandex-research/rtdl-num-embeddings/blob/main/package/rtdl_num_embeddings.py"""
+
+import math
+import warnings
+from collections import OrderedDict
+from typing import Any, Dict, List, Optional, Union, Callable, Type
+
+try:
+    import sklearn.tree as sklearn_tree
+except ImportError:
+    sklearn_tree = None
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.parameter import Parameter
+
+try:
+    from tqdm.auto import tqdm
+except ImportError:
+    tqdm = None
+
+
+class _Periodic(nn.Module):
+    """
+    WARNING: the direct usage of this module is discouraged
+    (do this only if you understand why this warning is here).
+    """
+
+    def __init__(self, n_features: int, k: int, sigma: float) -> None:
+        if sigma <= 0.0:
+            raise ValueError(f'sigma must be positive, however: {sigma=}')
+
+        super().__init__()
+        self._sigma = sigma
+        self.weight = Parameter(torch.empty(n_features, k))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # NOTE[DIFF]
+        # Here, extreme values (~0.3% probability) are explicitly avoided just in case.
+        # In the paper, there was no protection from extreme values.
+        bound = self._sigma * 3
+        nn.init.trunc_normal_(self.weight, 0.0, self._sigma, a=-bound, b=bound)
+
+    def forward(self, x: Tensor) -> Tensor:
+        if x.ndim < 2:
+            raise ValueError(
+                f'The input must have at least two dimensions, however: {x.ndim=}'
+            )
+
+        x = 2 * math.pi * self.weight * x[..., None]
+        x = torch.cat([torch.cos(x), torch.sin(x)], -1)
+        return x
+
+
+# _NLinear is a simplified copy of delu.nn.NLinear:
+# https://yura52.github.io/delu/stable/api/generated/delu.nn.NLinear.html
+class _NLinear(nn.Module):
+    """N *separate* linear layers for N feature embeddings."""
+
+    def __init__(self, n: int, in_features: int, out_features: int) -> None:
+        super().__init__()
+        self.weight = Parameter(torch.empty(n, in_features, out_features))
+        self.bias = Parameter(torch.empty(n, out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        d_in_rsqrt = self.weight.shape[-2] ** -0.5
+        nn.init.uniform_(self.weight, -d_in_rsqrt, d_in_rsqrt)
+        nn.init.uniform_(self.bias, -d_in_rsqrt, d_in_rsqrt)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.ndim == 3
+        assert x.shape[-(self.weight.ndim - 1) :] == self.weight.shape[:-1]
+        x = (x[..., None, :] @ self.weight).squeeze(-2)
+        x = x + self.bias
+        return x
+
+
+class PeriodicEmbeddings(nn.Module):
+    """PL & PLR & PLR(lite) (P ~ Periodic, L ~ Linear, R ~ ReLU) embeddings for continuous features.
+
+    **Shape**
+
+    - Input: `(*, n_features)`
+    - Output: `(*, n_features, d_embedding)`
+
+    **Examples**
+
+    >>> batch_size = 2
+    >>> n_cont_features = 3
+    >>> x = torch.randn(batch_size, n_cont_features)
+    >>>
+    >>> # PLR embeddings (by default, d_embedding=24).
+    >>> m = PeriodicEmbeddings(n_cont_features, lite=False)
+    >>> m(x).shape
+    torch.Size([2, 3, 24])
+    >>>
+    >>> # PLR(lite) embeddings.
+    >>> m = PeriodicEmbeddings(n_cont_features, lite=True)
+    >>> m(x).shape
+    torch.Size([2, 3, 24])
+    >>>
+    >>> # PL embeddings.
+    >>> m = PeriodicEmbeddings(n_cont_features, d_embedding=8, activation=False, lite=False)
+    >>> m(x).shape
+    torch.Size([2, 3, 8])
+    """  # noqa: E501
+
+    def __init__(
+        self,
+        n_features: int,
+        d_embedding: int = 24,
+        *,
+        n_frequencies: int = 48,
+        frequency_init_scale: float = 0.01,
+        activation: bool = True,
+        lite: bool,
+    ) -> None:
+        """
+        Args:
+            n_features: the number of features.
+            d_embedding: the embedding size.
+            n_frequencies: the number of frequencies for each feature.
+                (denoted as "k" in Section 3.3 in the paper).
+            frequency_init_scale: the initialization scale for the first linear layer
+                (denoted as "sigma" in Section 3.3 in the paper).
+                **This is an important hyperparameter**,
+                see the documentation for details.
+            activation: if True, the embeddings is PLR, otherwise, it is PL.
+            lite: if True, the last linear layer (the "L" step)
+                is shared between all features. See the README.md document for details.
+        """
+        super().__init__()
+        self.periodic = _Periodic(n_features, n_frequencies, frequency_init_scale)
+        self.linear: Union[nn.Linear, _NLinear]
+        if lite:
+            # NOTE[DIFF]
+            # The PLR(lite) variation was not covered in this paper about embeddings,
+            # but it was used in the paper about the TabR model.
+            if not activation:
+                raise ValueError('lite=True is allowed only when activation=True')
+            self.linear = nn.Linear(2 * n_frequencies, d_embedding)
+        else:
+            self.linear = _NLinear(n_features, 2 * n_frequencies, d_embedding)
+        self.activation = nn.ReLU() if activation else None
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Do the forward pass."""
+        if x.ndim < 2:
+            raise ValueError(
+                f'The input must have at least two dimensions, however: {x.ndim=}'
+            )
+
+        x = self.periodic(x)
+        x = self.linear(x)
+        if self.activation is not None:
+            x = self.activation(x)
+        return x
+
+    
+class MLP(nn.Module):
+    """The MLP model used in [gorishniy2021revisiting].
+
+    The following scheme describes the architecture:
+
+    .. code-block:: text
+
+          MLP: (in) -> Block -> ... -> Block -> Linear -> (out)
+        Block: (in) -> Linear -> Activation -> Dropout -> (out)
+
+    Examples:
+        .. testcode::
+
+            x = torch.randn(4, 2)
+            module = MLP.make_baseline(x.shape[1], [3, 5], 0.1, 1)
+            assert module(x).shape == (len(x), 1)
+
+    References:
+        * [gorishniy2021revisiting] Yury Gorishniy, Ivan Rubachev, Valentin Khrulkov, Artem Babenko, "Revisiting Deep Learning Models for Tabular Data", 2021
+    """
+
+    class Block(nn.Module):
+        """The main building block of `MLP`."""
+
+        def __init__(
+            self,
+            *,
+            d_in: int,
+            d_out: int,
+            bias: bool,
+            activation,
+            dropout: float,
+        ) -> None:
+            super().__init__()
+            self.linear = nn.Linear(d_in, d_out, bias)
+            self.activation = activation()
+            self.dropout = nn.Dropout(dropout)
+
+        def forward(self, x: Tensor) -> Tensor:
+            return self.dropout(self.activation(self.linear(x)))
+
+    def __init__(
+        self,
+        *,
+        d_in: int,
+        d_layers: List[int],
+        dropouts: Union[float, List[float]],
+        activation: Union[str, Callable[[], nn.Module]],
+        d_out: int,
+    ) -> None:
+        """
+        Note:
+            `make_baseline` is the recommended constructor.
+        """
+        super().__init__()
+        if isinstance(dropouts, float):
+            dropouts = [dropouts] * len(d_layers)
+        assert len(d_layers) == len(dropouts)
+
+        self.blocks = nn.Sequential(
+            *[
+                MLP.Block(
+                    d_in=d_layers[i - 1] if i else d_in,
+                    d_out=d,
+                    bias=True,
+                    activation=activation,
+                    dropout=dropout,
+                )
+                for i, (d, dropout) in enumerate(zip(d_layers, dropouts))
+            ]
+        )
+        self.head = nn.Linear(d_layers[-1] if d_layers else d_in, d_out)
+
+    @classmethod
+    def make_baseline(
+        cls: Type['MLP'],
+        d_in: int,
+        d_layers: List[int],
+        dropout: float,
+        d_out: int,
+    ) -> 'MLP':
+        """Create a "baseline" `MLP`.
+
+        This variation of MLP was used in [gorishniy2021revisiting]. Features:
+
+        * :code:`Activation` = :code:`ReLU`
+        * all linear layers except for the first one and the last one are of the same dimension
+        * the dropout rate is the same for all dropout layers
+
+        Args:
+            d_in: the input size
+            d_layers: the dimensions of the linear layers. If there are more than two
+                layers, then all of them except for the first and the last ones must
+                have the same dimension. Valid examples: :code:`[]`, :code:`[8]`,
+                :code:`[8, 16]`, :code:`[2, 2, 2, 2]`, :code:`[1, 2, 2, 4]`. Invalid
+                example: :code:`[1, 2, 3, 4]`.
+            dropout: the dropout rate for all hidden layers
+            d_out: the output size
+        Returns:
+            MLP
+
+        References:
+            * [gorishniy2021revisiting] Yury Gorishniy, Ivan Rubachev, Valentin Khrulkov, Artem Babenko, "Revisiting Deep Learning Models for Tabular Data", 2021
+        """
+        assert isinstance(dropout, float), 'In this constructor, dropout must be float'
+        if len(d_layers) > 2:
+            assert len(set(d_layers[1:-1])) == 1, (
+                'In this constructor, if d_layers contains more than two elements, then'
+                ' all elements except for the first and the last ones must be equal.'
+            )
+        return MLP(
+            d_in=d_in,
+            d_layers=d_layers,  # type: ignore
+            dropouts=dropout,
+            activation='ReLU',
+            d_out=d_out,
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.blocks(x)
+        x = self.head(x)
+        return x
+
+
+class ResNet(nn.Module):
+    """The ResNet model used in [gorishniy2021revisiting].
+
+    The following scheme describes the architecture:
+
+    .. code-block:: text
+
+        ResNet: (in) -> Linear -> Block -> ... -> Block -> Head -> (out)
+
+                 |-> Norm -> Linear -> Activation -> Dropout -> Linear -> Dropout ->|
+                 |                                                                  |
+         Block: (in) ------------------------------------------------------------> Add -> (out)
+
+          Head: (in) -> Norm -> Activation -> Linear -> (out)
+
+    Examples:
+        .. testcode::
+
+            x = torch.randn(4, 2)
+            module = ResNet.make_baseline(
+                d_in=x.shape[1],
+                n_blocks=2,
+                d_main=3,
+                d_hidden=4,
+                dropout_first=0.25,
+                dropout_second=0.0,
+                d_out=1
+            )
+            assert module(x).shape == (len(x), 1)
+
+    References:
+        * [gorishniy2021revisiting] Yury Gorishniy, Ivan Rubachev, Valentin Khrulkov, Artem Babenko, "Revisiting Deep Learning Models for Tabular Data", 2021
+    """
+
+    class Block(nn.Module):
+        """The main building block of `ResNet`."""
+
+        def __init__(
+            self,
+            *,
+            d_main: int,
+            d_hidden: int,
+            bias_first: bool,
+            bias_second: bool,
+            dropout_first: float,
+            dropout_second: float,
+            normalization,
+            activation,
+            skip_connection: bool,
+        ) -> None:
+            super().__init__()
+            self.normalization = normalization(d_main)
+            self.linear_first = nn.Linear(d_main, d_hidden, bias_first)
+            self.activation = activation()
+            self.dropout_first = nn.Dropout(dropout_first)
+            self.linear_second = nn.Linear(d_hidden, d_main, bias_second)
+            self.dropout_second = nn.Dropout(dropout_second)
+            self.skip_connection = skip_connection
+
+        def forward(self, x: Tensor) -> Tensor:
+            x_input = x
+            x = self.normalization(x)
+            x = self.linear_first(x)
+            x = self.activation(x)
+            x = self.dropout_first(x)
+            x = self.linear_second(x)
+            x = self.dropout_second(x)
+            if self.skip_connection:
+                x = x_input + x
+            return x
+
+    class Head(nn.Module):
+        """The final module of `ResNet`."""
+
+        def __init__(
+            self,
+            *,
+            d_in: int,
+            d_out: int,
+            bias: bool,
+            normalization,
+            activation,
+        ) -> None:
+            super().__init__()
+            self.normalization = normalization(d_in)
+            self.activation = activation()
+            self.linear = nn.Linear(d_in, d_out, bias)
+
+        def forward(self, x: Tensor) -> Tensor:
+            if self.normalization is not None:
+                x = self.normalization(x)
+            x = self.activation(x)
+            x = self.linear(x)
+            return x
+
+    def __init__(
+        self,
+        *,
+        d_in: int,
+        n_blocks: int,
+        d_main: int,
+        d_hidden: int,
+        dropout_first: float,
+        dropout_second: float,
+        normalization,
+        activation,
+        d_out: int,
+    ) -> None:
+        """
+        Note:
+            `make_baseline` is the recommended constructor.
+        """
+        super().__init__()
+
+        self.first_layer = nn.Linear(d_in, d_main)
+        if d_main is None:
+            d_main = d_in
+        self.blocks = nn.Sequential(
+            *[
+                ResNet.Block(
+                    d_main=d_main,
+                    d_hidden=d_hidden,
+                    bias_first=True,
+                    bias_second=True,
+                    dropout_first=dropout_first,
+                    dropout_second=dropout_second,
+                    normalization=normalization,
+                    activation=activation,
+                    skip_connection=True,
+                )
+                for _ in range(n_blocks)
+            ]
+        )
+        self.head = ResNet.Head(
+            d_in=d_main,
+            d_out=d_out,
+            bias=True,
+            normalization=normalization,
+            activation=activation,
+        )
+
+    @classmethod
+    def make_baseline(
+        cls: Type['ResNet'],
+        *,
+        d_in: int,
+        n_blocks: int,
+        d_main: int,
+        d_hidden: int,
+        dropout_first: float,
+        dropout_second: float,
+        d_out: int,
+    ) -> 'ResNet':
+        """Create a "baseline" `ResNet`.
+
+        This variation of ResNet was used in [gorishniy2021revisiting]. Features:
+
+        * :code:`Activation` = :code:`ReLU`
+        * :code:`Norm` = :code:`BatchNorm1d`
+
+        Args:
+            d_in: the input size
+            n_blocks: the number of Blocks
+            d_main: the input size (or, equivalently, the output size) of each Block
+            d_hidden: the output size of the first linear layer in each Block
+            dropout_first: the dropout rate of the first dropout layer in each Block.
+            dropout_second: the dropout rate of the second dropout layer in each Block.
+
+        References:
+            * [gorishniy2021revisiting] Yury Gorishniy, Ivan Rubachev, Valentin Khrulkov, Artem Babenko, "Revisiting Deep Learning Models for Tabular Data", 2021
+        """
+        return cls(
+            d_in=d_in,
+            n_blocks=n_blocks,
+            d_main=d_main,
+            d_hidden=d_hidden,
+            dropout_first=dropout_first,
+            dropout_second=dropout_second,
+            normalization='BatchNorm1d',
+            activation='ReLU',
+            d_out=d_out,
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.first_layer(x)
+        x = self.blocks(x)
+        x = self.head(x)
+        return x
