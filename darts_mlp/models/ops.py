a4e434d509b5aa0cdab6f8c427a7939011c80121
Arseniy Polyubin <apolyubin@npomis.ru>
2024-05-14 11:07:48 +0000

    Initial commit doesn't working yet

---
+++ darts_mlp/models/ops.py
@@ -0,0 +1,140 @@
+""" Operations """
+import torch
+import torch.nn as nn
+import genotypes as gt
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+
+OPS = {
+    'none': lambda in_size, out_size, dropout, affine: Zero(in_size),
+    'MLP_Relu': lambda in_size, out_size, dropout, affine: MLP(in_size, out_size, nn.ReLU(), dropout, affine),
+    'MLP_Tanh': lambda in_size, out_size, dropout, affine: MLP(in_size, out_size, nn.Tanh(), dropout, affine),
+    'MLP_Sigmoid': lambda in_size, out_size,dropout, affine: MLP(in_size, out_size, nn.Sigmoid(), dropout, affine),
+    'MLP_Gelu':  lambda in_size, out_size,dropout, affine: MLP(in_size, out_size, nn.GELU(), dropout, affine),
+    'skip_connect': lambda in_size, out_size, dropout, affine: nn.Identity(),
+}
+
+
+def drop_path_(x, drop_prob, training):
+    if training and drop_prob > 0.:
+        keep_prob = 1. - drop_prob
+        # per data point mask; assuming x in cuda.
+        mask = torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob)
+        x.div_(keep_prob).mul_(mask)
+
+    return x
+
+
+class DropPath_(nn.Module):
+    def __init__(self, p=0.):
+        """ [!] DropPath is inplace module
+        Args:
+            p: probability of an path to be zeroed.
+        """
+        super().__init__()
+        self.p = p
+
+    def extra_repr(self):
+        return 'p={}, inplace'.format(self.p)
+
+    def forward(self, x):
+        drop_path_(x, self.p, self.training)
+
+        return x
+
+# class FactorizedReduce(nn.Module):
+#     """
+#     Reduce feature map size by factorized pointwise(stride=2).
+#     """
+#     def __init__(self, C_in, C_out, affine=True):
+#         super().__init__()
+#         self.relu = nn.ReLU()
+#         self.conv1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+#         self.conv2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+#         self.bn = nn.BatchNorm2d(C_out, affine=affine)
+
+#     def forward(self, x):
+#         x = self.relu(x)
+#         out = torch.cat([self.conv1(x), self.conv2(x[:, :, 1:, 1:])], dim=1)
+#         out = self.bn(out)
+#         return out
+    
+# class TabDLModel(nn.Module):
+#     def __init__(self, n_cont_features, cat_cardinalities, mlp_kwargs, model_type, activation):
+#         super().__init__()
+#         self.cat_cardinalities = cat_cardinalities
+#         d_cat = sum(cat_cardinalities)
+#         self.activation = activation
+
+        
+#         if model_type == 'MLP-PLR':
+#             d_embedding = 24
+#             self.cont_embeddings = layers.PeriodicEmbeddings(n_cont_features, d_embedding, lite=False)
+#             d_num = n_cont_features * d_embedding
+#         elif model_type == 'MLP':
+#             self.cont_embeddings = nn.Identity()
+#             d_num = n_cont_features
+        
+#         self.backbone = layers.MLP(d_in=d_num + d_cat, **mlp_kwargs)
+
+#     def forward(self, x_cont, x_cat):
+#         x = []
+#         x.append(self.cont_embeddings(x_cont).flatten(1))
+#         if x_cat is not None:
+#             x.extend(
+#                 F.one_hot(column, cardinality)
+#                 for column, cardinality in zip(x_cat.T, self.cat_cardinalities)
+#             )
+#         x = torch.column_stack(x)
+#         x = self.backbone(x)
+#         return x
+
+
+class MLP(nn.Module):
+    def __init__(self, in_size, out_size, activation, dropout=True, affine=True):
+        super().__init__()
+        if dropout:
+            self.net = nn.Sequential(
+            nn.Linear(in_size, out_size),
+            activation,
+            nn.BatchNorm2d(out_size, affine=affine)
+            )
+        else:
+                        self.net = nn.Sequential(
+            nn.Linear(in_size, out_size),
+            activation,
+            nn.Dropout(),
+            nn.BatchNorm2d(out_size, affine=affine)
+            )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+
+class Zero(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+            return x * 0.
+
+
+class MixedOp(nn.Module):
+    """ Mixed operation """
+    def __init__(self, in_size, out_size, dropout,):
+        super().__init__()
+        self._ops = nn.ModuleList()
+        for primitive in gt.PRIMITIVES:
+            op = OPS[primitive](in_size, out_size, affine=False)
+            self._ops.append(op)
+
+    def forward(self, x, weights):
+        """
+        Args:
+            x: input
+            weights: weight for each operation
+        """
+        return sum(w * op(x) for w, op in zip(weights, self._ops))
---
+++ darts_mlp/utils.py
@@ -0,0 +1,211 @@
+""" Utilities """
+import os
+import logging
+import shutil
+import torch
+import numpy as np
+import sklearn 
+import torch.utils.data as data
+
+
+def get_data(dataset_name, batch_size, num_workers):
+    """ Get torchvision dataset """
+    if dataset_name == 'california':
+        dataset = sklearn.datasets.fetch_california_housing()
+        X_cont = dataset["data"]
+        Y = dataset["target"]
+        X_cont = X_cont.astype(np.float32)
+        n_cont_features = X_cont.shape[1]
+        Y = Y.astype(np.float32)
+        all_idx = np.arange(len(Y))
+
+        trainval_idx, test_idx = sklearn.model_selection.train_test_split(
+            all_idx, train_size=0.8, random_state=42
+        )
+        
+        train_idx, val_idx = sklearn.model_selection.train_test_split(
+            trainval_idx, train_size=0.8, random_state=42
+        )
+
+        X_train, y_train = X_cont[train_idx], Y[train_idx]
+        X_val, y_val = X_cont[val_idx], Y[val_idx]
+        X_test, y_test = X_cont[test_idx], Y[test_idx]
+
+        noise = np.random.default_rng(42).normal(0.0, 1e-5, X_train.shape).astype(X_train.dtype)
+        preprocessing = sklearn.preprocessing.QuantileTransformer(
+            n_quantiles=max(min(len(train_idx) // 30, 1000), 10),
+            output_distribution="normal",
+            subsample=10**9,
+        )
+        preprocessing.fit(X_train + noise)
+
+        X_train = preprocessing.transform(X_train)
+        X_val = preprocessing.transform(X_val)
+        X_test = preprocessing.transform(X_test)
+
+        y_mean, y_std = y_train.mean(), y_train.std()
+        y_train = (y_train - y_mean) / y_std
+        y_val = (y_val - y_mean) / y_std
+        y_test = (y_test - y_mean) / y_std
+
+        dataset_train = data.TensorDataset(torch.tensor(X_train), torch.tensor(y_train))
+        dataloader_train = data.DataLoader(
+            dataset=dataset_train,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+        dataset_val = data.TensorDataset(torch.tensor(X_val), torch.tensor(y_val))
+        dataloader_val = data.DataLoader(
+            dataset=dataset_val,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+        dataset_test = data.TensorDataset(torch.tensor(X_test), torch.tensor(y_test))
+        dataloader_test = data.DataLoader(
+            dataset=dataset_test,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+    elif dataset == 'covtype':
+        dataset_name = sklearn.datasets.fetch_covtype()
+        X_cont = dataset["data"][:, :10]
+        X_cat = dataset["data"][:, 10:]
+        Y = dataset["target"]
+        X_cont = X_cont.astype(np.float32)
+        X_cat = X_cat.astype(np.int64)
+        n_cont_features = X_cont.shape[1]
+        Y = Y.astype(np.int64) - 1
+
+        all_idx = np.arange(len(Y))
+        trainval_idx, test_idx = sklearn.model_selection.train_test_split(
+            all_idx, train_size=0.8, random_state=42
+        )
+        train_idx, val_idx = sklearn.model_selection.train_test_split(
+            trainval_idx, train_size=0.8, random_state=42
+        )
+
+        X_cont_train, X_cat_train, y_train = X_cont[train_idx], X_cat[train_idx], Y[train_idx]
+        X_cont_val, X_cat_val, y_val = X_cont[val_idx], X_cat[val_idx], Y[val_idx]
+        X_cont_test, X_cat_test, y_test = X_cont[test_idx], X_cat[test_idx], Y[test_idx]
+
+        noise = np.random.default_rng(42).normal(0.0, 1e-5, X_cont_train.shape).astype(X_cont_train.dtype)
+        preprocessing = sklearn.preprocessing.QuantileTransformer(
+            n_quantiles=max(min(len(train_idx) // 30, 1000), 10),
+            output_distribution="normal",
+            subsample=10**9,
+        )
+        preprocessing.fit(X_cont_train + noise)
+
+        X_cont_train = preprocessing.transform(X_cont_train)
+        X_cont_val = preprocessing.transform(X_cont_val)
+        X_cont_test = preprocessing.transform(X_cont_test)
+
+        dataset_train = data.TensorDataset(torch.tensor(X_cont_train), torch.tensor(X_cat_train), torch.tensor(y_train))
+        dataloader_train = data.DataLoader(
+            dataset=dataset_train,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+        dataset_val = data.TensorDataset(torch.tensor(X_cont_val), torch.tensor(X_cat_val), torch.tensor(y_val))
+        dataloader_val = data.DataLoader(
+            dataset=dataset_val,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+        dataset_test = data.TensorDataset(torch.tensor(X_cont_test), torch.tensor(X_cat_test), torch.tensor(y_test))
+        dataloader_test = data.DataLoader(
+            dataset=dataset_test,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=True,
+        )
+
+    return {
+    'train': dataloader_train,
+    'val': dataloader_val,
+    'test': dataloader_test
+}
+
+
+def get_logger(file_path):
+    """ Make python logger """
+    # [!] Since tensorboardX use default logger (e.g. logging.info()), we should use custom logger
+    logger = logging.getLogger('darts')
+    log_format = '%(asctime)s | %(message)s'
+    formatter = logging.Formatter(log_format, datefmt='%m/%d %I:%M:%S %p')
+    file_handler = logging.FileHandler(file_path)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
+
+
+def param_size(model):
+    """ Compute parameter size in MB """
+    n_params = sum(
+        np.prod(v.size()) for k, v in model.named_parameters() if not k.startswith('aux_head'))
+    return n_params / 1024. / 1024.
+
+
+class AverageMeter():
+    """ Computes and stores the average and current value """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        """ Reset all statistics """
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        """ Update statistics """
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """ Computes the precision@k for the specified values of k """
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    # one-hot case
+    if target.ndimension() > 1:
+        target = target.max(1)[1]
+
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(1.0 / batch_size))
+
+    return res
+
+
+def save_checkpoint(state, ckpt_dir, is_best=False):
+    filename = os.path.join(ckpt_dir, 'checkpoint.pth.tar')
+    torch.save(state, filename)
+    if is_best:
+        best_filename = os.path.join(ckpt_dir, 'best.pth.tar')
+        shutil.copyfile(filename, best_filename)
